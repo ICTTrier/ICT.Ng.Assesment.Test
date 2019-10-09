222efc4762a651946328d17850050e5bd631ad05
Sebastian Schulze-Khyi <s.schulze@its-cooperation.de>
2019-10-09 17:50:35 +0200

    ref/angular-major-upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,39 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  exchangeRates : {from:string;to:string}[] = [
-    {from:"BTC",to:"EUR"},
-    {from:"XVG",to:"EUR"},
-    {from:"ETH",to:"EUR"},
-    {from:"ADA",to:"EUR"},
-    {from:"EOS",to:"EUR"},
-    {from:"IOT",to:"EUR"},
-    {from:"TRX",to:"EUR"},
-    {from:"BTC",to:"USD"},
-    {from:"XVG",to:"USD"},
-    {from:"ETH",to:"USD"},
-    {from:"ADA",to:"USD"},
-    {from:"EOS",to:"USD"},
-    {from:"IOT",to:"USD"},
-    {from:"TRX",to:"USD"},
-    {from:"BTC",to:"GBP"},
-    {from:"XVG",to:"GBP"},
-    {from:"ETH",to:"GBP"},
-    {from:"ADA",to:"GBP"},
-    {from:"EOS",to:"GBP"},
-    {from:"IOT",to:"GBP"},
-    {from:"TRX",to:"GBP"}
-   ];
-  title = 'ict-ng-test';
+  public exchangeRates: { from: string; to: string }[] = [
+    {from: 'BTC', to: 'EUR'},
+    {from: 'XVG', to: 'EUR'},
+    {from: 'ETH', to: 'EUR'},
+    {from: 'ADA', to: 'EUR'},
+    {from: 'EOS', to: 'EUR'},
+    {from: 'IOT', to: 'EUR'},
+    {from: 'TRX', to: 'EUR'},
+    {from: 'BTC', to: 'USD'},
+    {from: 'XVG', to: 'USD'},
+    {from: 'ETH', to: 'USD'},
+    {from: 'ADA', to: 'USD'},
+    {from: 'EOS', to: 'USD'},
+    {from: 'IOT', to: 'USD'},
+    {from: 'TRX', to: 'USD'},
+    {from: 'BTC', to: 'GBP'},
+    {from: 'XVG', to: 'GBP'},
+    {from: 'ETH', to: 'GBP'},
+    {from: 'ADA', to: 'GBP'},
+    {from: 'EOS', to: 'GBP'},
+    {from: 'IOT', to: 'GBP'},
+    {from: 'TRX', to: 'GBP'}
+  ];
+  public title = 'ict-ng-test';
 
-  refresh() {
+  public refresh(): void {
     // little trick to copy the model to force angular to refresh the view
+    // TODO: should use ChangeDetectionStrategy and markForCheck
     this.exchangeRates = JSON.parse(JSON.stringify(this.exchangeRates));
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderCmpComponent } from './header-cmp/header-cmp.component';
 import { ContentCmpComponent } from './content-cmp/content-cmp.component';
-import { SolutionCmpComponent } from './solution-cmp/solution-cmp.component';
+// import { SolutionCmpComponent } from './solution-cmp/solution-cmp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderCmpComponent,
     ContentCmpComponent,
-    SolutionCmpComponent
+    // SolutionCmpComponent
   ],
   imports: [
     BrowserModule,
